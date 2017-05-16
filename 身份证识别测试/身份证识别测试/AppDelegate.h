2352b69367ffe70ac3931819958e74f1dd049a7a
czq <czq@hudd.com>
2017-05-16 19:57:53 +0800

    身份证识别

---
+++ 身份证识别测试/身份证识别测试/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  身份证识别测试
+//
+//  Created by 清风 on 2017/5/15.
+//  Copyright © 2017年 hdu.edu.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
